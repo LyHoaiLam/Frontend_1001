c0ab1c188c0826ca6c7274607db60d3b08b0b1ba
LyHoaiLam <hoailam03999@gmail.com>
2023-05-27 00:16:30 +0700

    JavaScript code Video

--- js/Page_01.js
+++ js/Page_01.js
@@ -44,14 +44,37 @@ window.addEventListener('scroll', scrollHeader)
 
 
 // Swiper Discover
-var swiper = new Swiper(".discover__container", {
+var swiper = new Swiper(".discover__container", {/*Kéo ảnh Discover The Most Attractive Places */
     effect: "coverflow",
     grabCursor: true,
     centeredSlides: true,
     slidesPerView: "auto",
-    loop: true,
-    spaceBetween: 32,
+    loop: true,/*Kéo ảnh không hết (hết rồi quay lại như vòng tròn) */
+    spaceBetween: 5,/*Khoảng trống 2 ảnh khéo */
     coverflowEffect: {
-      rotate: 0,
+      rotate: 50,   
     },
   });
+
+  //Video
+  const videoFile = document.getElementById('video-file'),
+  videoButton = document.getElementById('video-button'),
+  videoIcon = document.getElementById('video-icon')
+
+  function playPause(){
+    if(videoFile.paused){
+        //Play video
+        videoFile.play()
+        //we change the icon
+        videoIcon.classList.add('ri-pause-line')//Lấy trên swiper icon pause (class)
+        videoIcon.classList.remove('ri-play-line')
+    }else{
+        //Pause video
+        videoFile.pause()
+        //We change the icon
+        videoIcon.classList.remove('ri-pause-line')
+        videoIcon.classList.add('ri-play-line')
+    }
+  }
+
+  videoButton.addEventListener('click', playPause)
