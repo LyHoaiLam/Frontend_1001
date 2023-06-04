4c0308efc776aeceffb9cef8670db79bcadfaeb6
LyHoaiLam <hoailam03999@gmail.com>
2023-06-05 01:34:41 +0700

    add code Page_01

--- js/Page_01.js
+++ js/Page_01.js
@@ -1,21 +1,21 @@
 
-const navMenu = document.getElementById('nav-menu'), //lấy id
-    navToggle = document.getElementById('nav-toggle'), // idicon toggle
-    navClose = document.getElementById('nav-close')// id icon close
+const navMenu = document.getElementById('nav-menu'),
+navToggle = document.getElementById('nav-toggle'),
+navClose = document.getElementById('nav-close')
 
-//Menu show
-if(navToggle){//Click icon vào hiện ra Menu
-    navToggle.addEventListener('click', () =>{
+// Menu Show
+if(navToggle){
+    navToggle.addEventListener('click', () => {
         navMenu.classList.add('show-menu')
     })
 }
 
-//Menu Hideen
-if(navClose){//Click icon vào hiện ra Menu
+if(navClose){
     navClose.addEventListener('click', () =>{
         navMenu.classList.remove('show-menu')
     })
 }
+
 
 // click hidden and show liên tục thì thay đổi class nav__menu + class show-menu và ngược lại mất class show-menu
 if(navClose){
@@ -25,13 +25,11 @@ if(navClose){
 }
 
 // Remove menu Mobile
-
 const navLink = document.querySelectorAll('.nav__link')// Home, About, Discover, Place
 function linkAction(){
     const navMenu = document.getElementById('nav-menu')
     navMenu.classList.remove('show-menu')
 }
-
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 //Change Background Header
@@ -39,9 +37,7 @@ function scrollHeader(){
     const header = document.getElementById('header')
     if(this.scrollY >= 100) header.classList.add('scroll=header'); else nav.classList.remove('scroll-header')
 }
-
 window.addEventListener('scroll', scrollHeader)
-
 
 // Swiper Discover
 var swiper = new Swiper(".discover__container", {/*Kéo ảnh Discover The Most Attractive Places */
@@ -56,11 +52,12 @@ var swiper = new Swiper(".discover__container", {/*Kéo ảnh Discover The Most 
     },
   });
 
+
+
   //Video
   const videoFile = document.getElementById('video-file'),
   videoButton = document.getElementById('video-button'),
   videoIcon = document.getElementById('video-icon')
-
   function playPause(){
     if(videoFile.paused){
         //Play video
@@ -76,5 +73,12 @@ var swiper = new Swiper(".discover__container", {/*Kéo ảnh Discover The Most 
         videoIcon.classList.add('ri-play-line')
     }
   }
-
   videoButton.addEventListener('click', playPause)
+
+  function finalVideo(){
+    //Video and, icon change
+    videoIcon.classList.remove('ri-pause-line')
+    videoIcon.classList.add('ri-play-line')
+  }
+  //end, whe the video ends
+  videoFile.addEventListener('ended', finalVideo)
